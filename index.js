c0f8f7f4b1b05b20e0d6e1ee165c4f18b1d03b18
Stefven Torichie <64120065+torichiestefven@users.noreply.github.com>
2020-11-11 14:35:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  const doodler = document.createElement('div')
+  
+  function createDoodler() {
+    grid.appendChild(doodler)
+    doodler.classList.add('doodler')
+    doodlerLeftSpace = platforms[0].left
+    doodler.style.left = doodlerLeftSpace + 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+  }
+  }
+  start()
+})
