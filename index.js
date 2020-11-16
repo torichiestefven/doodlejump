495fbe0673ebe9c5bb3a6dc99436ede1cb76e925
Stefven Torichie <64120065+torichiestefven@users.noreply.github.com>
2020-11-16 23:22:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
   }
-  
+  function createDoodler() {
+    grid.appendChild(doodler)
+    doodler.classList.add('doodler')
+    doodlerLeftSpace = platforms[0].left
+    doodler.style.left = doodlerLeftSpace + 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+  }
   }
   start()
 })
