06ef96f0caa33826fb2fd7376b84a70bc3715f59
Stefven Torichie <64120065+torichiestefven@users.noreply.github.com>
2020-11-21 16:19:33 +0000

    add move left feature

--- index.js
+++ index.js
@@ -128,3 +128,18 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   start()
 })
+
+function moveLeft() {
+    if (isGoingRight) {
+        clearInterval(rightTimerId)
+        isGoingRight = false
+    }
+    isGoingLeft = true
+    leftTimerId = setInterval(function () {
+        if (doodlerLeftSpace >= 0) {
+          console.log('going left')
+          doodlerLeftSpace -=5
+           doodler.style.left = doodlerLeftSpace + 'px'
+        } else moveRight()
+    },20)
+  }
